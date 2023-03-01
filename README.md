51d39da4981b663fd81e597669beba8b0672805c
Burca Ciprian <burcaciprian@gmail.com>
2023-03-01 20:40:15 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FWDFakeStoreAPIClient
+
+A description of this package.
---
+++ Sources/FWDFakeStoreAPIClient/FWDFakeStoreAPIClient.swift
@@ -0,0 +1,6 @@
+public struct FWDFakeStoreAPIClient {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
